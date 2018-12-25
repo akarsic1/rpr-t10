4eebbef63ca82ae3c473fbc89bd33571981b1eb7
Amel <akarsic1@etf.unsa.ba>
2018-12-25 09:04:23 +0100

    Klasa grad kreirana.

---
+++ src/ba/unsa/etf/rpr/Grad.java
@@ -0,0 +1,26 @@
+package ba.unsa.etf.rpr;
+
+public class Grad {
+
+    public void setNaziv(String naziv) {
+
+    }
+
+    public void setBrojStanovnika(int i) {
+    }
+
+    public void setDrzava(Drzava drzava) {
+    }
+
+    public String getNaziv() {
+        return null;
+    }
+
+    public int getBrojStanovnika() {
+        return 0;
+    }
+
+    public Grad getDrzava() {
+        return null;
+    }
+}
