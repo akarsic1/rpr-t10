55e0f3e64662e454a5fbb36ead7f054ea53a7260
Amel <akarsic1@etf.unsa.ba>
2018-12-25 09:02:39 +0100

    Klasa drzava kreirana.

---
+++ src/ba/unsa/etf/rpr/Drzava.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr;
+
+public class Drzava {
+    public void setNaziv(String bosna_i_hercegovina) {
+    }
+
+    public String getNaziv() {
+        return null;
+    }
+
+    public void setGlavniGrad(Grad sarajevo) {
+    }
+}
